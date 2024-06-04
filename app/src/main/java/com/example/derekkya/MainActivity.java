7ea78230a25ba586f6f35ff08de5ad43397dcdcc
derekky <derek.vieira.s@gmail.com>
2024-06-04 15:19:41 -0300

    Adicionado Permissões de localização. Criação da função de cálculo da distância entre dois pontos.

--- app/src/main/java/com/example/derekkya/MainActivity.java
+++ app/src/main/java/com/example/derekkya/MainActivity.java
@@ -20,5 +20,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Variáveis de Geolocalização
+        //lat2 e lon2 equivale ao campus USP-Leste por enquanto
+        //lat1 e lon1 será as coordenadas atuais
+        double lat1 = 0, lat2 = -23.48514333234679, lon1 = 0, lon2 = -46.50123767388998;
+
+    }
+
+    public double calcularDistancia(double lat1, double lat2, double lon1, double lon2){
+
+        lon1 = Math.toRadians(lon1);
+        lon2 = Math.toRadians(lon2);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers.
+        double r = 6371;
+
+        // calculate the result
+        return(c * r);
     }
 }
